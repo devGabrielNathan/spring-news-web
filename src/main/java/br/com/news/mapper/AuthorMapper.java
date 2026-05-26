888b35a82352168ceac4e4a3c5b5b40d68f581d8
Gabriel Nathan dos Santos Pires <devgabrielnathan@gmail.com>
2026-05-26 20:15:09 -0300

    feat(author): add repository and mapper
    - Implement AuthorRepository extending JpaRepository
    
    - Add existsByEmail query method
    
    - Implement AuthorMapper with entity-DTO conversion
    
    - Add partial update support via updateEntityFromPatch

--- src/main/java/br/com/news/mapper/AuthorMapper.java
+++ src/main/java/br/com/news/mapper/AuthorMapper.java
@@ -1,4 +1,75 @@
 package br.com.news.mapper;
 
+import br.com.news.dto.AuthorPatchRequest;
+import br.com.news.dto.AuthorRequest;
+import br.com.news.dto.AuthorResponse;
+import br.com.news.entity.AuthorEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Objects;
+
+@Component
 public class AuthorMapper {
+    public AuthorEntity toEntity(AuthorRequest authorRequest) {
+        return new AuthorEntity(
+                null,
+                authorRequest.getName(),
+                authorRequest.getEmail(),
+                authorRequest.getPassword(),
+                authorRequest.getBirthDate(),
+                authorRequest.getEducation(),
+                authorRequest.getSignature(),
+                authorRequest.getStatus(),
+                authorRequest.isEditor()
+        );
+    }
+
+    public void updateEntityFromPatch(AuthorEntity authorEntity, AuthorPatchRequest authorPatchRequest) {
+        if (Objects.nonNull(authorPatchRequest.getName())) {
+            authorEntity.setName(authorPatchRequest.getName());
+        }
+
+        if (Objects.nonNull(authorPatchRequest.getEmail())) {
+            authorEntity.setEmail(authorPatchRequest.getEmail());
+        }
+
+        if (Objects.nonNull(authorPatchRequest.getBirthDate())) {
+            authorEntity.setBirthDate(authorPatchRequest.getBirthDate());
+        }
+
+        if (Objects.nonNull(authorPatchRequest.getEducation())) {
+            authorEntity.setEducation(authorPatchRequest.getEducation());
+        }
+
+        if (Objects.nonNull(authorPatchRequest.getSignature())) {
+            authorEntity.setSignature(authorPatchRequest.getSignature());
+        }
+        if (Objects.nonNull(authorPatchRequest.getStatus())) {
+            authorEntity.setStatus(authorPatchRequest.getStatus());
+        }
+        if (Objects.nonNull(authorPatchRequest.getIsEditor())) {
+            authorEntity.setEditor(authorPatchRequest.getIsEditor());
+        }
+
+    }
+
+    public AuthorResponse toResponse(AuthorEntity authorEntity) {
+        return new AuthorResponse(
+                authorEntity.getId(),
+                authorEntity.getName(),
+                authorEntity.getEmail(),
+                authorEntity.getBirthDate(),
+                authorEntity.getEducation(),
+                authorEntity.getSignature(),
+                authorEntity.getStatus(),
+                authorEntity.isEditor()
+        );
+    }
+
+
+
+    public List<AuthorResponse> toResponseList(List<AuthorEntity> authorEntities) {
+        return authorEntities.stream().map(this::toResponse).toList();
+    }
 }
--- src/main/java/br/com/news/repository/AuthorRepository.java
+++ src/main/java/br/com/news/repository/AuthorRepository.java
@@ -1,4 +1,8 @@
 package br.com.news.repository;
 
-public class AuthorRepository {
+import br.com.news.entity.AuthorEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<AuthorEntity, Long> {
+    boolean existsByEmail(String email);
 }
